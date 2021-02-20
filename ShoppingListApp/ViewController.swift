5201cf6c761c9cf904f92bb3a9b47fe693c78327
Вадим <vadim_ponomarev_1989@mail.ru>
2021-02-20 22:01:18 +0300

    Initial Commit

---
+++ ShoppingListApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ShoppingListApp
+//
+//  Created by Вадим on 20.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
